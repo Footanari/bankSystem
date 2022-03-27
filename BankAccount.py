14d44f7c1ba891e5ace34fb01f482bdb9e773857
Al1002 <71392792+Al1002@users.noreply.github.com>
2022-03-27 15:59:24 +0300

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -1,13 +1,18 @@
 class account:
-    def __init__(self, name):
+    def __init__(self, name, balance):
         self.__name = name
-        self.__balance = 0
+        self.__balance = balance
         self.__history = []
         self.__history_id = 0
+        self.__loan = 0
+    
+        @property 
+    def name(self):
+        print(self.__name)
     
     def print_all_transactions(self):
         print(self.__history)
-
+    
     def deposit(self, ammount):
         self.__balance += ammount
         self.__history_id += 1
@@ -19,4 +24,14 @@ class account:
         self.__balance -= ammount
         self.__history_id += 1
         self.__history.append("transaction N"+str(self.__history_id)+", type � withdraw, amount - "+str(ammount)+", new balance - "+str(self.__balance))
+    
+    def return_loan_payment(self, amount):
+        if(self.__balance<ammount):
+            raise Exception("return ammount greater than balance")
+        if(self.__loan<ammount):
+            ammount=self.__loan
+        self.__balance -= ammount
+        self.__loan -= ammount
+        self.__history_id += 1
+        self.__history.append("transaction N"+str(self.__history_id)+", type � loan payment, amount - "+str(ammount)+", new balance - "+str(self.__balance),", new loan - "+str(self.__loan))
 
