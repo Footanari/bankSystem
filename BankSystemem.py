1b41a6ce90c324f9c91e0c031e119e434d919471
Footanari <nigslayrr@gmail.com>
2022-03-27 16:35:40 +0300

    added give loan

--- BankSystemem.py
+++ BankSystemem.py
@@ -1,15 +1,16 @@
-from BankAccount import account as bank
+from BankAccount import account
 
 class system:
     accounts = []
     moneyInBank = 0
-    def Register_new_user(new_Bank_Account:bank.account): #( new_Bank_Account : BankAcconut )
+    def Register_new_user(new_Bank_Account:account): #( new_Bank_Account : BankAcconut )
         system.accounts.append()
         return len(system.accounts) - 1
     def Delete_user(user_id):
         system.accounts.remove(system.accounts[user_id])
     def Give_loan_to_user(user_id, amount, interest, return_period_in_months):
-        pass
+        account.loan += amount + interest
+        account.balance += amount
     def Show_all_transactions(user_id = ""):
         if user_id == "":
             for user in system.accounts:
